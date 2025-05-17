8d661eeb5420697be0cb8777b197a1a2c6a22fbc
Aman Devrath <amndev94@gmail.com>
2025-05-17 11:37:04 -0700

    17-5-2025 11.35am- created dict and appended in post request

--- Python/FastAPI/main.py
+++ Python/FastAPI/main.py
@@ -11,13 +11,26 @@ class Post(BaseModel):
     published: bool = True          #True if no value specified
     rating: Optional[int] = None    #None or empty if no value specified. Optional value of int type, no other data type
 
+my_posts = [
+            {
+                "title": "title of post 2",
+                "content": "title of post 2",
+                "id": 1
+            },
+            {
+                "title": "my favorite place",
+                "content": "I like japan",
+                "id": 2
+            }
+           ]
+
 @app.get("/")
 def root():
     return {"message":"Hello, World!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"This is your post"}
+    return {"data":my_posts}
 
 #without defining model (Class Post is a model with parameters)
 '''@app.post("/createposts")
@@ -25,14 +38,8 @@ def get_posts():
       result = f"title: {payload['title']}, content : {payload['content']}"
       return {result}'''
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    # print(new_post)
-    print(new_post.title)
-    print(new_post.content)
-    print(new_post.published)
-    print(new_post.rating)
-    return {"message":"post created."}
-            
-#pydantic usage
-#title str, content str
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    my_posts.append(post_dict)
+    return {"data":post_dict}
